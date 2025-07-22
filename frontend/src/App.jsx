fb962d6b1ad5c143a8f3721c0e133d910bc51ce8
Ampreeth S <ampreeth02@gmail.com>
2025-07-22 19:56:53 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Home from "./pages/user/Home";
+import AdminDashboard from "./components/admin/AdminDashboard";
+import './index.css';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
